d5f0e38c89f41ec9dd680386d2d7167885e26c33
Kseniia_O <liprekqn@gmail.com>
2023-05-20 17:43:47 +0300

    After 1st review

--- homework.py
+++ homework.py
@@ -1,18 +1,17 @@
-import os
-from dotenv import load_dotenv
-import requests
-import time
-import telegram
-import sys
-import local_exceptions
 import logging
+import os
+import sys
+import time
+from http import HTTPStatus
 
-# Ниже объяснение что это
-# from logs_settings import LOGS_SETTINGS
+import requests
+import telegram
+from dotenv import load_dotenv
+
+import local_exceptions
 
 
 load_dotenv()
-
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -20,217 +19,182 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-RETRY_PERIOD = 10 * 60
-
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-# Очень захотелось реализовать идею хранить все настройки для логов в одном
-# файле и потом их импортировать в код.
-# Понимаю, что код в том файле весь будет исполнятся, сидела думала над идеей
-# нэйм равно мэйн, но ведь я хочу видеть логи со всего проекта
-# тоже.. Интересно мнение на этот счет
+RETRY_PERIOD = 10 * 60
 
-# Однако, чтобы пройти пайтест приходится оставлять идею в комментариях.
-# Поэтому логи в коде начинаются с HW_LOGGER.
-
-# HW_LOGGER = LOGS_SETTINGS.get('hw_logger')
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename='logs_all.log',
-    filemode='w',
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
-HW_LOGGER = logging.getLogger('homework.py')
-HW_LOGGER.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(stream=sys.stdout)
-formatter = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(message)s'
-)
-handler.setFormatter(formatter)
-HW_LOGGER.addHandler(handler)
+# Text messages:
+LOGS_OK = 'OK'
+LOGS_END = '--- end of file ---'
+# func check_tokens
+TOKENS_LOGS_START = 'Проверка токенов'
+NO_TOKENS_LOGS = 'Нет токена в {name}'
+NO_TOKENS_RAISE = 'Потеряли токен(ы), проверь логи.'
+# func send_message
+MESSAGE_LOGS_START = 'Отправляем сообщение в ТГ...'
+MESSAGE_SENT_LOGS = 'Сообщение успешно отправлено. {message}'
+MESSAGE_NOT_SENT_LOGS = 'Ошибка при отправке сообщения: {error}'
+# func get_api_answer
+API_LOGS_START = 'Проверка запроса к API'
+API_BAD_REQUEST_RAISE = ('Сбой в запросе к API: {error}. '
+                         'Отправили данные: {data_for_api}')
+API_NOT200_RAISE = ('Ответ не 200, а {status_code}. '
+                    'Отправили данные: {data_for_api}')
+API_BAD_JSON_RAISE = ('Ошибка сервера {code}: {error}. '
+                      'Отправили данные: {data_for_api}')
+# func check_response
+RESPONSE_LOGS_START = 'Проверка ответа API'
+RESPONCE_NOT_DICT_RAISE = 'Ответ от API не словарь, a {type}'
+RESPONSE_NO_HOMEWORKS_RAISE = 'Потеряли ключ homeworks в ответе API'
+RESPONSE_HOMEWORKS_NOT_LIST_RAISE = 'Домашка это не список, a {type}'
+# func parse_status
+STATUS_LOGS_START = 'Проверка статуса домашней работы'
+STATUS_UNKNOWN_NAME_RAISE = 'Неизвестное имя у работы: {homework_name}'
+STATUS_UNKNOWN_RAISE = 'Неизвестный статус у работы: {status}'
+STATUS_NOT_IN_VERDICTS_RAISE = 'Статуса {status} нет в HOMEWORK_VERDICTS'
+STATUS_MESSAGE = ('Изменился статус проверки работы '
+                  '"{homework_name}". {verdict}')
+# func main LOGS
+MAIN_LOGS_START = '--- beginning of file ---'
+MAIN_NO_UPDATES = 'Новых домашек/ошибок нет'
+MAIN_MESSAGE_NOT_SENT = 'Не получилось отправить сообщение в ТГ: {error}'
+MAIN_MESSAGE_NO_UPDATES = 'Это мы уже отправляли'
+MAIN_ERROR_MESSAGE = 'Хьюстон, у нас проблемы: {error}'
+MAIN_ERROR_MESSAGE_NOT_SENT = ('Не получилось отправить сообщение '
+                               'об ошибке в ТГ: {error}')
+MAIN_ERROR_MESSAGE_NO_UPDATES = 'Сообщение об ошибке уже отправляли'
 
 
 def check_tokens():
     """Переменные окружения доступны."""
-    HW_LOGGER.debug(
-        'Проверка токенов')
+    logging.debug(TOKENS_LOGS_START)
 
-    tokens = (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)
-    for token in tokens:
+    TOKENS = {
+        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
+    }
+    for name, token in TOKENS.items():
         if token is None:
-            HW_LOGGER.critical(
-                f'Нет токена в {token}')
-            raise ValueError(
-                'Потеряли токен')
-    HW_LOGGER.debug('OK')
-    return True
+            logging.critical(NO_TOKENS_LOGS.format(
+                name=name)
+            )
+    for token in TOKENS.values():
+        if token is None:
+            raise ValueError(NO_TOKENS_RAISE)
+
+    logging.debug(LOGS_OK)
 
 
 def send_message(bot, message):
     """Отправка сообщений в Telegram."""
-    HW_LOGGER.debug(
-        'Отправляем сообщение в ТГ...')
+    logging.debug(MESSAGE_LOGS_START)
 
     try:
         bot.send_message(
             chat_id=TELEGRAM_CHAT_ID,
             text=message)
-        HW_LOGGER.debug(
-            'Сообщение успешно отправлено')
-    except telegram.TelegramError:
-        HW_LOGGER.error(
-            'Ошибка при отправке сообщения')
+        logging.debug(MESSAGE_SENT_LOGS.format(
+            message=message)
+        )
+    except telegram.TelegramError as error:
+        logging.exception(MESSAGE_NOT_SENT_LOGS.format(
+            error=error)
+        )
 
 
 def get_api_answer(timestamp):
     """Запрос к эндпоинту API-сервиса."""
-    HW_LOGGER.debug(
-        'Проверка запроса к API')
+    logging.debug(API_LOGS_START)
 
-    if 'Authorization' not in HEADERS:
-        HW_LOGGER.error(
-            'Нет ключа Authorization в HEADERS')
-        raise KeyError(
-            'Забыли как авторизовываться, скоро вспомним')
-
-    try:
-        data_for_api = {
-            'url': ENDPOINT,
-            'headers': HEADERS,
-            'params': {'from_date': timestamp},
-        }
-    except NameError:
-        HW_LOGGER.error(
-            'Данные для запроса к API не верны')
-        raise NameError(
-            'Потеряли данные, скоро найдем')
-
+    data_for_api = {
+        'url': ENDPOINT,
+        'headers': HEADERS,
+        'params': {'from_date': timestamp},
+    }
     try:
         response = requests.get(**data_for_api)
-        if response.status_code != 200:
-            HW_LOGGER.error(
-                'Страница недоступна')
-            raise local_exceptions.Not200Error(
-                'Страница отвечает с кодом отличным от 200')
-    except Exception as error:
-        HW_LOGGER.error(
-            f'ЛЮБОЙ ДРУГОЙ СБОЙ: {error}')
-        raise local_exceptions.APIError(
-            f'ЛЮБОЙ ДРУГОЙ СБОЙ: {error}')
+    except requests.RequestException as error:
+        raise local_exceptions.APIError(API_BAD_REQUEST_RAISE.format(
+            error=error,
+            data_for_api=data_for_api)
+        )
+    if response.status_code != HTTPStatus.OK:
+        raise requests.HTTPError(API_NOT200_RAISE.format(
+            status_code=response.status_code,
+            data_for_api=data_for_api)
+        )
+    response = response.json()
+    if 'code' in response or 'error' in response:
+        raise requests.HTTPError(API_BAD_JSON_RAISE.format(
+            code=response.get('code'),
+            error=response.get('error'),
+            data_for_api=data_for_api)
+        )
 
-    HW_LOGGER.debug('OK')
-    return response.json()
+    logging.debug(LOGS_OK)
+    return response
 
 
 def check_response(response):
     """Ответ API соответствует документации."""
-    HW_LOGGER.debug(
-        'Проверка ответа API')
+    logging.debug(RESPONSE_LOGS_START)
 
     if not isinstance(response, dict):
-        HW_LOGGER.error(
-            'Ответ от API не словарь')
-        raise TypeError(
-            'Ответ от API не словарь')
-
-    main_keys = [
-        'homeworks',
-        'current_date',
-    ]
-    for key in main_keys:
-        if key not in response:
-            HW_LOGGER.error(
-                f'Нет ключа {key} в ответе API')
-            raise KeyError(
-                f'Потеряли ключ {key} в ответе API')
+        raise TypeError(RESPONCE_NOT_DICT_RAISE.format(
+            type=type(response))
+        )
+    if 'homeworks' not in response:
+        raise KeyError(RESPONSE_NO_HOMEWORKS_RAISE)
 
     homeworks = response.get('homeworks')
 
     if not isinstance(homeworks, list):
-        HW_LOGGER.error(
-            'Домашка это не список')
-        raise TypeError(
-            'Домашка это не список')
+        raise TypeError(RESPONSE_HOMEWORKS_NOT_LIST_RAISE.format(
+            type=type(homeworks))
+        )
 
-    HW_LOGGER.debug('OK')
+    logging.debug(LOGS_OK)
     return homeworks
 
 
 def parse_status(homework):
     """Статус домашней работы."""
-    HW_LOGGER.debug(
-        'Проверка статуса домашней работы')
-
-    # Воюю с пайтестом, мне кажется логичнее эту часть прописать в функции выше
-    # проверяем же, что документации соответствует ответ ответа
-    try:
-        homework_keys = [
-            'id',
-            'status',
-            'homework_name',
-            'reviewer_comment',
-            'date_updated',
-            'lesson_name',
-        ]
-        for key in homework_keys:
-            if key not in homework:
-                HW_LOGGER.error(
-                    f'Нет ключа {key} в homeworks')
-                raise KeyError(
-                    f'Потеряли ключ {key} у домашки, ищем')
-    except Exception:
-        key == []
-        if key in homework:
-            HW_LOGGER.debug(
-                'Домашек еще не отправляли на проверку')
-
-    verdict_keys = [
-        'approved',
-        'reviewing',
-        'rejected',
-    ]
-    for key in verdict_keys:
-        if key not in HOMEWORK_VERDICTS:
-            HW_LOGGER.error(
-                f'Нет ключа {key} в HOMEWORK_VERDICTS')
-            raise KeyError(
-                f'Потеряли ключ {key}, скоро найдем')
+    logging.debug(STATUS_LOGS_START)
 
     homework_name = homework.get('homework_name')
     status = homework.get('status')
 
     if homework_name not in homework.values():
-        HW_LOGGER.error(
-            f'Странное имя у работы: {homework_name}, нам такого не задавали!')
-        raise local_exceptions.UnknownNameError(
-            'Неизвестное имя у работы')
-
-    if status not in HOMEWORK_VERDICTS.keys():
-        HW_LOGGER.error(
-            f'Непонятный статус у работы: {status}, срочно писать всем!!1!')
-        raise local_exceptions.UnknownStatusError(
-            f'Неизвестный статус у работы: {status}')
+        raise KeyError(STATUS_UNKNOWN_NAME_RAISE.format(
+            homework_name=homework_name)
+        )
+    if status not in homework.values():
+        raise KeyError(STATUS_UNKNOWN_RAISE.format(
+            status=status)
+        )
+    if status not in HOMEWORK_VERDICTS:
+        raise KeyError(STATUS_NOT_IN_VERDICTS_RAISE.format(
+            status=status)
+        )
 
     verdict = HOMEWORK_VERDICTS.get(status)
-    message = f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    message = STATUS_MESSAGE.format(
+        homework_name=homework_name,
+        verdict=verdict)
 
-    HW_LOGGER.debug('OK')
+    logging.debug(LOGS_OK)
     return message
 
 
-def main():
+def main():  # noqa C901
     """Основная логика работы бота."""
-    HW_LOGGER.debug(
-        '--- beggining of file ---')
+    logging.debug(MAIN_LOGS_START)
 
-    if not check_tokens():
-        sys.exit()
-
+    check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     # timestamp = 0
@@ -240,48 +204,64 @@ def main():
     while True:
 
         try:
+
             response = get_api_answer(timestamp)
             homeworks = check_response(response)
 
             if not homeworks:
-                HW_LOGGER.debug(
-                    'Новых домашек/ошибок нет')
+                logging.debug(MAIN_NO_UPDATES)
                 continue
 
             message = parse_status(homeworks[0])
 
             if last_message is not message and error_message:
-                send_message(bot, message)
-                last_message = message
-                timestamp = int(time.time())
+                try:
+                    send_message(bot, message)
+                    last_message = message
+                    timestamp = response.get('current_date', timestamp)
+                except Exception as error:
+                    logging.exception(MAIN_MESSAGE_NOT_SENT.format(
+                        error=error)
+                    )
             else:
-                HW_LOGGER.debug(
-                    'Это мы уже отправляли')
+                logging.debug(MAIN_MESSAGE_NO_UPDATES)
 
-        except (
-            local_exceptions.APIError,
-            local_exceptions.UnknownNameError,
-            local_exceptions.UnknownStatusError,
-            local_exceptions.Not200Error,
-            TypeError,
-            KeyError,
-            NameError,
-        ) as error:
+        except Exception as error:
 
             if last_message is not error_message:
-                error_message = f'Ошибка!! {error}'
-                send_message(bot, error_message)
-                last_message = error_message
-                timestamp = int(time.time())
+                error_message = MAIN_ERROR_MESSAGE.format(
+                    error=error)
+                logging.exception(error_message)
+                try:
+                    send_message(bot, error_message)
+                    last_message = error_message
+                except Exception as error:
+                    logging.exception(MAIN_ERROR_MESSAGE_NOT_SENT.format(
+                        error=error)
+                    )
             else:
-                HW_LOGGER.debug(
-                    'Сообщение об ошибке уже отправляли')
+                logging.debug(MAIN_ERROR_MESSAGE_NO_UPDATES)
 
         finally:
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    # Logs settings
+    logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=(
+            logging.StreamHandler(stream=sys.stdout),
+            logging.FileHandler(
+                filename=__file__ + '.log',
+                mode='w')
+        ),
+        format='%(asctime)s - '
+               '%(levelname)s - '
+               '%(message)s - '
+               '%(name)s - '
+               '%(funcName)s - '
+               '%(lineno)d'
+    )
     main()
-    HW_LOGGER.debug(
-        '--- end of file ---')
+    logging.debug(LOGS_END)
--- local_exceptions.py
+++ local_exceptions.py
@@ -1,21 +1,3 @@
-class UnknownStatusError(Exception):
-    """Неизвестный статус домашки."""
-
-    ...
-
-
-class UnknownNameError(Exception):
-    """Неизвестное имя домашки."""
-
-    ...
-
-
-class Not200Error(Exception):
-    """Не 200."""
-
-    ...
-
-
 class APIError(Exception):
     """ЛЮБОЙ ДРУГОЙ СБОЙ :'D."""
 
